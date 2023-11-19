a46218ec0b737d03928f1fb642e821a0bdd98877
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2023-11-19 05:06:40 +0000

    all

---
+++ backend/filwuha/models/admin.py
@@ -0,0 +1,20 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from filwuha.models import db
+
+
+class Admin(db.Model, UserMixin):
+    __tablename__ = "admins"
+
+    admin_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    # def check_password(self, password):
+    #     return check_password_hash(self.password_hash, password)
---
+++ backend/filwuha/models/order.py
@@ -0,0 +1,35 @@
+from filwuha.models import db
+
+
+class Order(db.Model):
+    __tablename__ = "orders"
+
+    order_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(45), nullable=False)
+    last_name = db.Column(db.String(45), nullable=False)
+    email = db.Column(db.String(45), nullable=True)
+    phone_number = db.Column(db.String(45), nullable=False)
+    order_date = db.Column(db.DateTime, nullable=False)
+    order_time = db.Column(db.DateTime, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    payment = db.Column(db.Boolean, nullable=False)
+
+    def __init__(
+        self,
+        first_name,
+        last_name,
+        email,
+        phone_number,
+        order_date,
+        order_time,
+        price,
+        payment,
+    ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
+        self.order_date = order_date
+        self.order_time = order_time
+        self.price = price
+        self.payment = payment
