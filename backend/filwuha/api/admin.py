47026ffe162ef8d7ab470140a6cf870a1788edbc
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2023-11-20 07:27:45 +0300

    Add POST And PUT method for admin and order moder API

--- backend/filwuha/api/admin.py
+++ backend/filwuha/api/admin.py
@@ -1,14 +1,19 @@
-from flask import jsonify
+from flask import jsonify, request, abort
 from flask import Blueprint
+from flask_login import login_user
 from filwuha.models import Order
+from filwuha.models import Admin
 
 admin_bp = Blueprint("admin", __name__, url_prefix="/api/v1")
 
-
-@admin_bp.route("/admin/login", methods=["POST"])
-def admin_login():
-    return True
-
+def validate_request(required_fields):
+    data = request.get_json()
+    if not data:
+        abort(400, description="No input data provided")
+    missing_fields = [field for field in required_fields if field not in data]
+    if missing_fields:
+        abort(400, description=f"Missing fields: {', '.join(missing_fields)}")
+    return data
 
 @admin_bp.route("/admin/orders")
 def get_orders():
@@ -38,3 +43,28 @@ def delete_order(id):
         return jsonify({"message": "Order successfully deleted"})
     except Exception as e:
         return jsonify(Error=str(e)), 404
+
+@admin_bp.route("/admin/login", methods=["POST"], strict_slashes=False)
+def login():
+    required_fields = ["username", "password_hash"]
+    data = validate_request(required_fields)
+    admin_obj = Admin.query.filter_by(username=data["username"]).first()
+    if not admin_obj or not admin_obj.check_password(data["password"]):
+        abort(401, description="Invalid username or password")
+    login_user(admin_obj)
+    return jsonify(admin_obj.serilaize()), 200
+
+@admin_bp.route("/admin/orders/<id>", methods=["PUT"], strict_slashes=False)
+def update_order(id):
+    required_fields = ["first_name", "last_name", "phone_number", "order_date", "order_time", "price", "payment"]
+    data = validate_request(required_fields)
+    order_obj = Order.query.get(id)
+    order_obj.first_name = data["first_name"]
+    order_obj.last_name = data["last_name"]
+    order_obj.phone_number = data["phone_number"]
+    order_obj.order_date = data["order_date"]
+    order_obj.order_time = data["order_time"]
+    order_obj.price = data["price"]
+    order_obj.payment = data["payment"]
+    order_obj.update()
+    return jsonify(order_obj.serialize()), 200
--- backend/filwuha/api/book.py
+++ backend/filwuha/api/book.py
@@ -1,8 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, request, abort, jsonify
+from filwuha.models import Order
 
 book_bp = Blueprint("book", __name__, url_prefix="/api/v1")
 
-@book_bp.route("/book", methods=["POST"])
-def order():
- return "Booked"
-
+@book_bp.route("/book", methods=["POST"], strict_slashes=False)
+def create_book():
+    if not request.get_json():
+        abort(400, description="No input data provided")
+    if "user_name" or "last_name" or "phone_number" or "order_date" or "order_time" or "price" or "payment" not in request.get_json():
+        abort(400, description="Missing input data")
+    data = request.get_json()
+    new_book = Order(
+        first_name=data["first_name"],
+        last_name=data["last_name"],
+        email=data["email"],
+        phone_number=data["phone_number"],
+        order_date=data["order_date"],
+        order_time=data["order_time"],
+        price=data["price"],
+        payment=data["payment"],
+    )
+    return jsonify(new_book.serialize()), 201
