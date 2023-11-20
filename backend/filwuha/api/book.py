8f73f3043ecb6ae8a18b41dfad60b38600a56865
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2023-11-20 09:07:14 +0300

    Add try except block

--- backend/filwuha/api/book.py
+++ backend/filwuha/api/book.py
@@ -9,15 +9,18 @@ def create_book():
         abort(400, description="No input data provided")
     if "user_name" or "last_name" or "phone_number" or "order_date" or "order_time" or "price" or "payment" not in request.get_json():
         abort(400, description="Missing input data")
-    data = request.get_json()
-    new_book = Order(
-        first_name=data["first_name"],
-        last_name=data["last_name"],
-        email=data["email"],
-        phone_number=data["phone_number"],
-        order_date=data["order_date"],
-        order_time=data["order_time"],
-        price=data["price"],
-        payment=data["payment"],
-    )
-    return jsonify(new_book.serialize()), 201
+    try:
+        data = request.get_json()
+        new_book = Order(
+            first_name=data["first_name"],
+            last_name=data["last_name"],
+            email=data["email"],
+            phone_number=data["phone_number"],
+            order_date=data["order_date"],
+            order_time=data["order_time"],
+            price=data["price"],
+            payment=data["payment"],
+        )
+        return jsonify(new_book.serialize()), 201
+    except Exception as e:
+        abort(400, description=str(e))
