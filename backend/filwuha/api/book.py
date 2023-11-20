88516192a6943b02ea8fff74248285e761e1d72b
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2023-11-20 09:17:53 +0300

    Add try except block

--- backend/filwuha/api/book.py
+++ backend/filwuha/api/book.py
@@ -5,11 +5,11 @@ book_bp = Blueprint("book", __name__, url_prefix="/api/v1")
 
 @book_bp.route("/book", methods=["POST"], strict_slashes=False)
 def create_book():
-    if not request.get_json():
-        abort(400, description="No input data provided")
-    if "user_name" or "last_name" or "phone_number" or "order_date" or "order_time" or "price" or "payment" not in request.get_json():
-        abort(400, description="Missing input data")
     try:
+        if not request.get_json():
+            abort(400, description="No input data provided")
+        if "user_name" or "last_name" or "phone_number" or "order_date" or "order_time" or "price" or "payment" not in request.get_json():
+            abort(400, description="Missing input data")
         data = request.get_json()
         new_book = Order(
             first_name=data["first_name"],
@@ -23,4 +23,4 @@ def create_book():
         )
         return jsonify(new_book.serialize()), 201
     except Exception as e:
-        abort(400, description=str(e))
+        abort(500, description=str(e))
