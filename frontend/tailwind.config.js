c65f6373d7799cb989f2e3978db3f6af2211096f
Zekaria Mohammed <zakisu250@gmail.com>
2023-11-21 16:47:05 +0300

    home page design

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryDark: '#1976D2',
+        primaryLight: '#BBDEF8',
+        primaryText: '#212121',
+        primaryColor: '#00BCD4',
+        textIcon: '#ffffff',
+        accentColor: '#FF9800',
+        secondaryText: '#757575',
+        dividerColor: '#BDBDBD',
+      },
+    },
   },
   plugins: [],
 };
