43a6c965fc4a0cd8812e9145888691d0f0793081
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2023-11-21 15:03:08 +0300

    Add Navbar with style

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,24 +3,26 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark bg-gray-500 p-4 py-4 px-3">
+    <nav className="bg-gray-300 mt-4 mx-5">
       <div className="container mx-auto flex justify-between items-center">
-        <Link
-          to="/"
-          className="flex items-center py-4 px-8 text-black hover:text-gray-400"
-        >
-          <h1 className="text-2xl font-bold">FILEWUHA</h1>
+        <Link to="/" className="flex items-center py-3 px-8">
+          <h1 className="text-4xl font-bold text-blue-950 hover:text-gray-400">
+            FILEWUHA
+          </h1>
         </Link>
         <ul className="flex space-x-4">
           <li>
-            <Link to="/" className="py-4 px-8 text-black hover:text-gray-400">
+            <Link
+              to="/"
+              className="text-blue-950 font-bold py-4 px-8 text-lg hover:text-gray-400"
+            >
               Home
             </Link>
           </li>
           <li>
             <Link
               to="/services"
-              className="py-4 px-8 text-black hover:text-gray-400"
+              className="text-blue-950 font-bold py-4 px-8 text-lg hover:text-gray-400"
             >
               Services
             </Link>
@@ -28,7 +30,7 @@ function Navbar() {
           <li>
             <Link
               to="/about"
-              className="py-4 px-8 text-black hover:text-gray-400"
+              className="text-blue-950 font-bold py-4 px-8 text-lg hover:text-gray-400"
             >
               About
             </Link>
@@ -36,7 +38,7 @@ function Navbar() {
           <li>
             <Link
               to="/contact"
-              className="py-4 px-8 text-black hover:text-gray-400"
+              className="text-blue-950 font-bold py-4 px-8 text-lg hover:text-gray-400"
             >
               Contact Us
             </Link>
