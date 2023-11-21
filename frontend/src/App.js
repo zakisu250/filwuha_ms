8571bdf23e3c90079151d6e5867c471a3b3e479b
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2023-11-21 11:38:33 +0300

    setup frontend with tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import Home from './components/Home';
-// import About from './components/About';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Services from "./components/pages/Services";
+import Contact from "./components/pages/Contact";
+import Book from "./components/pages/Book";
+import NotFound from "./components/pages/NotFound";
+import "./assets/styles/App.css";
+import React from "react";
 
 function App() {
-return (
-  <BrowserRouter>
-  <Navbar />
-  <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/about" element={<About />} />
-  </Routes>
-  </BrowserRouter>
-);
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
