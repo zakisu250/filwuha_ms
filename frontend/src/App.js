f50f6b6e4d35ccbfa57e43de9d58c7044420d094
Zekaria Mohammed <zakisu250@gmail.com>
2023-11-22 11:57:11 +0300

    added Payment page and Routing for Payment page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import Navbar from "./components/Navbar";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Services from "./components/pages/Services";
-import Contact from "./components/pages/Contact";
-import Book from "./components/pages/Book";
-import NotFound from "./components/pages/NotFound";
-import "./assets/styles/App.css";
-import React from "react";
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Services from './components/pages/Services';
+import Contact from './components/pages/Contact';
+import Book from './components/pages/Book';
+import NotFound from './components/pages/NotFound';
+import './assets/styles/App.css';
+import React from 'react';
+import Payment from './components/pages/Payment';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/book" element={<Book />} />
+        <Route path="/payment" element={<Payment />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/components/pages/Payment.js
@@ -0,0 +1,4 @@
+function Payment() {
+  return <div>Payment</div>;
+}
+export default Payment;
