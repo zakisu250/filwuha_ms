2195b8a325ac7546edac9dfec5e43083fea4d85d
zakisu250 <zakisu250@gmail.com>
2023-11-26 12:49:20 +0300

    components and edit

--- frontend/src/components/pages/Contact.js
+++ frontend/src/components/pages/Contact.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React from 'react';
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+      <div className="flex justify-center items-center h-full">
+        <div className="flex mt-20 w-full h-full">
+          <div className="flex justify-center items-center text-center p-5 w-full mx-10 mt-10">
+            <div className="flex flex-1 w-full justify-center items-center mx-auto h-full">
+              <span className="bg-[url('../../assets/images/entrance.png')] bg-no-repeat bg-center w-full h-full bg-contain"></span>
+            </div>
+            <div className="flex flex-col flex-1 w-full">
+              <iframe
+                title="Google maps to filwuha"
+                className="w-full h-400 border-none shadow-lg"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.6820580738336!2d38.752721800000005!3d9.019175699999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b859532793527%3A0x4312dd42c25a3146!2sFilwoha%20Hotel!5e0!3m2!1sen!2set!4v1629297816917!5m2!1sen!2set"
+              ></iframe>
+              <h1 className="text-center text-2xl mt-5">
+                ለማንኛውም ጥያቄዎች እባክዎን በዚህ ኢሜል ይላኩ
+              </h1>
+              <h1 className="text-center text-2xl">
+                For any inquiries please contact us with this email address
+              </h1>
+              <h1 className="text-center text-2xl mt-5 text-orange-700 font-bold">
+                spaserviceenterprise@gmail.com
+              </h1>
+              <div className="items-center mt-10 p-5 w-full transition-all">
+                <ul className="flex gap-2 justify-center items-center w-full">
+                  <li className="hover:scale-110 rounded-full bg-gray-400 px-2">
+                    <a href="https://www.facebook.com/filwuhaagelglot/">
+                      <i class="fa-brands fa-facebook-f text-2xl p-2"></i>
+                    </a>
+                  </li>
+                  <li className="hover:scale-110 rounded-full bg-gray-400 px-2">
+                    <a href="https://www.youtube.com/channel/UCXOvZZzH6OF_OdxjqxAqhrA">
+                      <i class="fa-brands fa-youtube text-2xl p-2"></i>
+                    </a>
+                  </li>
+                  <li className="hover:scale-110 rounded-full bg-gray-400 px-2">
+                    <a href="https://t.me/filwuhaagelglot">
+                      <i class="fa-brands fa-telegram text-2xl p-2"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
--- frontend/src/components/pages/Services.js
+++ frontend/src/components/pages/Services.js
@@ -1,7 +1,83 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Services() {
-  return <div>Services</div>;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col mt-20">
+        <h1 className="text-5xl font-bold text-center mt-10">Our Services</h1>
+        <ul className="mt-10 flex flex-col px-20">
+          <li className="flex bg-center w-full h-96 mt-10">
+            <span className="w-1/2 flex-1 bg-[url('../../assets/images/shower.jpg')] bg-cover bg-center rounded-xl shadow-xl"></span>
+            <div className="flex flex-col flex-1 justify-center items-center text-center p-5">
+              <h1 className="text-5xl mb-5">Shower / Bathing</h1>
+              <p className="text-3xl p-5">
+                Inside Finfine (old location) and the new Filwuha, you can relax
+                your mind by showering or bathing. This service is give to
+                individuals or customers who come with their families.{' '}
+              </p>
+              <p className="text-2xl p-5"></p>
+            </div>
+          </li>
+
+          <li className="flex bg-center w-full h-96 mt-20">
+            <div className="flex flex-col flex-1 justify-center items-center text-center p-5">
+              <h1 className="text-5xl">Hydro-massage</h1>
+              <p className="text-3xl p-5">
+                This service uses hydro-massage hot tab to relax the muscles and
+                create good blood flow. Athletic injuries, Arteritis and
+                Tendinitis can also be cured with this service as it is the
+                recommended type of medication. Hydro-massage also lowers
+                Anxiety and stress.
+              </p>
+              <p className="text-2xl p-5"></p>
+            </div>
+            <span className="w-1/2 flex-1 bg-[url('../../assets/images/bath.jpg')] bg-cover bg-center rounded-xl shadow-xl"></span>
+          </li>
+
+          <li className="flex bg-center w-full h-96 mt-20">
+            <span className="w-1/2 flex-1 bg-[url('../../assets/images/sauna.png')] bg-cover bg-center rounded-xl shadow-xl"></span>
+            <div className="flex flex-col flex-1 justify-center items-center text-center p-5">
+              <h1 className="text-5xl">Sauna</h1>
+              <p className="text-3xl p-5">
+                Sauna-bath helps in relaxing our body more. Even more it helps
+                to improve our cardiovascular activities. Athletics, gym fellows
+                and exercise instructors recommend Sauna because it helps relax
+                and release toxics out of the body after a good exercise.
+              </p>
+              <p className="text-2xl p-5"></p>
+            </div>
+          </li>
+
+          <li className="flex bg-center w-full h-96 mt-20">
+            <div className="flex flex-col flex-1 justify-center items-center text-center p-5">
+              <h1 className="text-5xl">Steam-bath</h1>
+              <p className="text-3xl p-5">
+                Steam-bath is a type of bathing where a steam generator is used
+                to produce water vapour. This service is used to cure
+                respiratory diseases such as asthma, bronchitis, sinusitis and
+                allergies. Steam-bath also helps to improve blood circulation
+                and metabolism.
+              </p>
+              <p className="text-2xl p-5"></p>
+            </div>
+            <span className="w-1/2 flex-1 bg-[url('../../assets/images/steam-bath.jpg')] bg-cover bg-center rounded-xl shadow-xl"></span>
+          </li>
+          <li className="flex flex-1 flex-col justify-center items-center mt-20 bg-accentColor rounded-2xl mb-10 p-5 shadow-lg">
+            <h1 className="text-5xl px-5 py-1">Checkout our packages</h1>
+            <div className="flex justify-between w-1/2">
+              <Link className="flex flex-col justify-center items-center p-5 bg-gray-200 rounded-xl border mx-5 mt-5 w-full hover:scale-105 hover:text-2xl hover:font-bold transition-all">
+                <h1 className="text-3xl text-center">Sauna-bath package</h1>
+              </Link>
+              <Link className="flex flex-col justify-center items-center p-5 bg-gray-200 rounded-xl border mx-5 mt-5 w-full hover:scale-105 hover:text-2xl hover:font-bold transition-all">
+                <h1 className="text-3xl text-center">Steam-bath package</h1>
+              </Link>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Services;
