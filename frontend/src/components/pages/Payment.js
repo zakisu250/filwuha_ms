bd385251cc9a6a7071e71211c3cc8f89ef9c9c66
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2023-11-27 22:10:35 +0300

    change the font for the payment page

--- frontend/src/components/pages/Payment.js
+++ frontend/src/components/pages/Payment.js
@@ -85,10 +85,8 @@ function Payment() {
 
   return (
     <div className="flex">
-      <div className="flex flex-col justify-center items-center h-1/2 w-1/3 mx-auto mt-32 p-5 border border-y-primaryText">
-        <h3 className="font-bold text-center text-4xl mb-10 w-full">
-          Payment Page
-        </h3>
+      <div className="flex flex-col justify-center items-center h-1/2 w-1/3 mx-auto mt-28 p-8 border mb-2 border-y-primaryText">
+        <h4 className="text-center text-4xl mb-10 w-full">Payment Page</h4>
         <p className="text-left w-full mb-2 font-bold">Order Summary:</p>
         <ul className="text-left w-full">
           <li className="flex justify-between py-2">
@@ -102,7 +100,6 @@ function Payment() {
             <p>Product:</p>
             <span>{formData.service}</span>
           </li>
-
           {formData.email && (
             <li className="flex justify-between py-2">
               <p>Email:</p>
@@ -129,6 +126,10 @@ function Payment() {
             <p>Slot:</p>
             <span>{formData.slot}</span>
           </li>
+          <li className="flex justify-between py-2">
+            <p>Account:</p>
+            <span>CBE: 123456789</span>
+          </li>
 
           <li className="flex justify-between py-2">
             <p>Price:</p>
@@ -142,7 +143,7 @@ function Payment() {
         </ul>
         <p>{paymentStatus}</p>
         <button
-          className="w-1/3 my-5 px-8 py-1 rounded-lg bg-slate-300 hover:bg-slate-500 hover:text-white transition-all"
+          className="w-1/3 justify-end mt-3 mb-auto py-1 rounded-lg bg-slate-300 hover:bg-slate-500 hover:text-white transition-all"
           disabled={isLoading}
           onClick={handlePay}
         >
