896d365369b0919d68b28794b4f3217810adc9d6
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2023-11-27 10:48:33 +0300

    Add Admin Dashbord and Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Navbar from "./components/Navbar";
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Services from './components/pages/Services';
-import Contact from './components/pages/Contact';
-import Book from './components/pages/Book';
-import NotFound from './components/pages/NotFound';
-import './assets/styles/App.css';
-import React from 'react';
-import Payment from './components/pages/Payment';
+import React from "react";
+import "./assets/styles/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Services from "./components/pages/Services";
+import Contact from "./components/pages/Contact";
+import Book from "./components/pages/Book";
+import NotFound from "./components/pages/NotFound";
+import Payment from "./components/pages/Payment";
+import AdminLogin from "./components/pages/AdminLogin";
+import AdminPage from "./components/pages/AdminPage";
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/book" element={<Book />} />
         <Route path="/payment" element={<Payment />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin/home" element={<AdminPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
